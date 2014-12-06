5bec9027854d1bff3ae454a257fcc2d7c87ea3ac
ejsd1989 <eissajamil@gmail.com>
2014-12-06 15:16:59 -0600

    app.js

--- dlDemo/app.js
+++ dlDemo/app.js
@@ -227,7 +227,7 @@ config(['$routeProvider', function($routeProvider) {
                                         return 'arm-state';
 										
 									case 'weather':
-										return 'current-temperature';
+										return 'status';
 										
                                 }
                             };
