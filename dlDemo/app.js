fed1d4c134cd19d3877701194268486c344a1f00
ejsd1989 <eissajamil@gmail.com>
2014-12-06 15:11:37 -0600

    app.js

--- dlDemo/app.js
+++ dlDemo/app.js
@@ -227,7 +227,7 @@ config(['$routeProvider', function($routeProvider) {
                                         return 'arm-state';
 										
 									case 'weather':
-										return 'current-temperature' + 'humidity' + 'wind-speed';
+										return 'current-temperature';
 										
                                 }
                             };
